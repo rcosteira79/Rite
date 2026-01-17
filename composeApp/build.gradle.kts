07f2e0253715c42122b084ab610554ff271f77c6
Ricardo Costeira <ricardo.costeira@mindera.com>
2026-01-17 01:11:30 +0100

    Added immutable collections dependency and updated one of the state classes

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -44,6 +44,7 @@ kotlin {
             implementation(libs.androidx.lifecycle.viewmodelCompose)
             implementation(libs.androidx.lifecycle.runtimeCompose)
             implementation(libs.kotlinx.datetime)
+            implementation(libs.kotlinx.collections.immutable)
             implementation(libs.sqldelight.coroutines.extensions)
         }
         commonTest.dependencies {
--- composeApp/src/commonMain/kotlin/com/ricardocosteira/habitlock/presentation/ui/calendar/CalendarState.kt
+++ composeApp/src/commonMain/kotlin/com/ricardocosteira/habitlock/presentation/ui/calendar/CalendarState.kt
@@ -1,6 +1,8 @@
 package com.ricardocosteira.habitlock.presentation.ui.calendar
 
 import com.ricardocosteira.habitlock.presentation.models.CalendarDayUiModel
+import kotlinx.collections.immutable.ImmutableList
+import kotlinx.collections.immutable.persistentListOf
 import kotlinx.datetime.LocalDate
 import kotlinx.datetime.Month
 
@@ -10,7 +12,7 @@ import kotlinx.datetime.Month
 data class CalendarState(
     val currentMonth: Month = Month.JANUARY,
     val currentYear: Int = 2026,
-    val days: List<CalendarDayUiModel> = emptyList(),
+    val days: ImmutableList<CalendarDayUiModel> = persistentListOf(),
     val isLoading: Boolean = true,
     val selectedDay: LocalDate? = null,
     val perfectDaysCount: Int = 0,
